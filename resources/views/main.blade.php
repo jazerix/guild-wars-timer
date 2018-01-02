00f1055fbbfcd85847bcf19a6ea48ee613bc70a1
Niels Faurskov <jazerixthegreat@gmail.com>
2018-01-02 05:17:04 +0100

    Production Build

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -11,7 +11,7 @@
         <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
         <link rel="stylesheet" href="{{ asset('css/main.css') }}" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
-        <title>Laravel</title>
+        <title>Live GW2</title>
     </head>
     <body>
         <div id="app">
